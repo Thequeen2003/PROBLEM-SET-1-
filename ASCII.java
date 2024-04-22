09680b6334866a6e0a3c4c31fa0203b82e17f608
Alantis Green <112572029+Thequeen2003@users.noreply.github.com>
2024-04-22 15:59:24 -0400

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,133 @@
+/**********************************************************************
+ 2 *AscII . j a v a
+ 3 *This something I HAD TO GO ON YOUTUBE TO LEARN.  i PERSONALLY hAD NO idea
+ 4 *@Alantis Green
+ 5 *@date : 8/29/2022
+ 6 *@ a c k n o w l e d g e m e n t : Alantis Green
+ 7 ***********************************************************************/
+public class ASCII {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        while (true) {
+
+            System.out.println(" O ");
+            System.out.println("   ");
+            System.out.println("___");
+
+            Thread.sleep(100);
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println(" | ");
+            System.out.println(" O ");
+            System.out.println("___");
+
+            Thread.sleep(100);
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("  ");
+            System.out.println("   ");
+            System.out.println("_O_");
+
+            Thread.sleep(100);
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("   ");
+            System.out.println("   ");
+            System.out.println("_<>_");
+
+            Thread.sleep(100);
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("  ");
+            System.out.println("   ");
+            System.out.println("_O_");
+
+            Thread.sleep(100);
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("  ");
+            System.out.println(" O ");
+            System.out.println("_|_");
+
+            Thread.sleep(100);
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("     _        ____    \n" +
+                    "U  /\"\\  u U /\"___|u  \n" +
+                    " \\/ _ \\/  \\| |  _ /  \n" +
+                    " / ___ \\   | |_| |   \n" +
+                    "/_/   \\_\\ _ \\____|_  \n" +
+                    " \\\\    >>(\")_)(|_(\") \n" +
+                    "(__)  (__)\"(__)__)\"    ");
+
+
+
+
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
